9f78280416e0ca3b30ca333a3c9bf3266582473a
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-10-01 21:16:12 -0400

    src/app.rs:tidy up before template

--- src/app.rs
+++ src/app.rs
@@ -13,21 +13,15 @@ impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            label: "OpenDyslexic".to_owned(),
+            value: 3.0,
         }
     }
 }
 
 impl TemplateApp {
-    /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
         setup_custom_fonts(&cc.egui_ctx);
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
@@ -37,19 +31,13 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
                 #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
                 {
@@ -66,11 +54,9 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
+            ui.heading("OpenDyslexic");
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
+                ui.label("OpenDyslexic: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
@@ -82,7 +68,7 @@ impl eframe::App for TemplateApp {
             ui.separator();
 
             ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
+                "https://github.com/gnostr-org/eframe_template/blob/master/",
                 "Source code."
             ));
 
@@ -95,7 +81,7 @@ impl eframe::App for TemplateApp {
 }
 
 fn setup_custom_fonts(ctx: &egui::Context) {
-    // Start with the default fonts (we will be adding to them rather than replacing them).
+
     let mut fonts = egui::FontDefinitions::default();
 
     fonts.font_data.insert(
@@ -122,8 +108,6 @@ fn setup_custom_fonts(ctx: &egui::Context) {
             "opendyslexic/OpenDyslexic-Bold-Italic.otf"
         )),
     );
-
-
 
     fonts
         .families
@@ -161,7 +145,6 @@ fn setup_custom_fonts(ctx: &egui::Context) {
         .or_default()
         .insert(0, "OD-Bold-Italic".to_owned());
 
-    // Tell egui to use these fonts:
     ctx.set_fonts(fonts);
 }
 
@@ -172,8 +155,8 @@ fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
         ui.hyperlink_to("egui", "https://github.com/emilk/egui");
         ui.label(" and ");
         ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
+            "OpenDyslexic",
+            "https://github.com/antijingoist/opendyslexic",
         );
         ui.label(".");
     });
